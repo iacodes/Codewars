ea9a59ad90cb5234b22328fcacd9a533567e7b88
iacodes <102711045+iacodes@users.noreply.github.com>
2022-06-17 22:41:31 -0700

    added function find the sum of row in triangle constituting of odd numbers

---
+++ sumOfOddNums.js
@@ -0,0 +1,28 @@
+/*Given the triangle of consecutive odd numbers
+
+1
+ 3     5
+7     9    11
+13    15    17    19
+21    23    25    27    29
+...
+
+Example:
+Calculate the sum of the numbers in the nth row of this triangle (starting at index 1) e.g.: (Input --> Output)
+
+1 -->  1
+2 --> 3 + 5 = 8
+
+*/
+
+function rowSumOddNumbers(n) {
+
+//   let sum = 0
+//   if (n%2!=0){
+//     for (i=0; i< n; i++){
+//       n**3
+//     }
+//     sum=n**2 + n**2-2
+//   }
+    return n**3
+}
