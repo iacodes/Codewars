dbbbc36afea230f8d3ce40d46107ccfaf1f93e4c
iacodes <102711045+iacodes@users.noreply.github.com>
2022-06-19 13:52:38 -0700

    added simple function to find complementary DNA string

---
+++ complimentaryDNA.js
@@ -0,0 +1,23 @@
+/*Deoxyribonucleic acid (DNA) is a chemical found in the nucleus of cells and carries the "instructions" for the development and functioning of living organisms.
+
+In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". Your function receives one side of the DNA (string, except for Haskell); you need to return the other complementary side. DNA strand is never empty or there is no DNA at all (again, except for Haskell).
+*/
+
+function DNAStrand(dna){
+  let complDNA = []
+  dna.split("").forEach( x=> {
+    if (x == "A"){
+      complDNA.push("T")
+    }
+    else  if (x == "T"){
+      complDNA.push("A")
+    }
+    else if (x == "C"){
+      complDNA.push("G")
+    }
+    else if (x == "G"){
+      complDNA.push("C")
+    }
+  })
+  return complDNA.join("")
+}
