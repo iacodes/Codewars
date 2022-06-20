1a8f4c4bf2fac5ea54286fc7573741842e7d543a
iacodes <102711045+iacodes@users.noreply.github.com>
2022-06-20 16:09:46 -0700

    added simple function to check if a year is leap year or not

---
+++ leapYearValidator.js
@@ -0,0 +1,11 @@
+/*
+determine, whether a given year is a leap year or not. In case you don't know the rules, here they are:
+
+years divisible by 4 are leap years
+but years divisible by 100 are not leap years
+but years divisible by 400 are leap year
+*/
+
+function isLeapYear(year) {
+  return ( (year%4 == 0) && (year%100!=0)) ? true : (year%400==0) ? true:false
+}
