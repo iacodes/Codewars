ff607e88cb44dfd5b2e416c2c0b5fde7f16701cf
iacodes <102711045+iacodes@users.noreply.github.com>
2022-07-07 22:51:47 -0700

    initial commit, function to replace value with new value every n values

---
+++ replaceNthCharacter.js
@@ -0,0 +1,29 @@
+/*
+Write a method, that replaces every nth char oldValue with char newValue.
+
+Inputs
+text: the string to modify
+n: change the target letter every nth occurrencies
+old_value (or similar): the targetted character
+new_value (or similar): the character to use as replacement
+Note for untyped languages: all inputs are always valid and of their expected type.
+
+Rules
+Your method has to be case sensitive!
+If n is 0 or negative or if it is larger than the count of the oldValue, return the original text without a change.
+*/
+
+
+function replaceNth(text, n, oldValue, newValue) {
+  let occ = 1;
+
+  return text.split("").map(x => {
+    if (x === oldValue){
+       if (occ%n==0 && n>1){
+         x=newValue;
+       }
+      occ++
+    }
+    return x
+  }).join("")
+}
