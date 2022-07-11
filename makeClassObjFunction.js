77d509eedd3045fb56f73271bc634a9c7f5aee2a
iacodes <102711045+iacodes@users.noreply.github.com>
2022-07-10 23:36:19 -0700

    initial commit for function to make class from function expression

--- makeClassObjFunction.js
+++ makeClassObjFunction.js
@@ -15,6 +15,8 @@ Create a function makeClass to allow creating objects like this
 const Animal = makeClass("name","species","age","health","weight","color")
 */
 
+/*
+
 function makeClass(name,species,age,health,weight,color) {
     class Animal{
         constructor(name,species,age,health,weight,color){
@@ -28,3 +30,43 @@ function makeClass(name,species,age,health,weight,color) {
     }
     return new Animal(name,species,age,health,weight,color)
 }
+*/
+class Animal{
+  constructor (name,species,age,health,weight,color){
+    this.name = name;
+    this.species = species;
+    this.age = age;
+    this.health = health;
+    this.weight = weight;
+    this.color = color;
+  }
+}
+
+function makeClass(...properties) {
+  return function ( ...arguments ) {
+    properties.forEach( (properties, item) => this[properties] = arguments[item] )
+  };
+}
+
+
+/*Alternative solutions
+function makeClass(...params) {
+  return class {
+    constructor(...args) {
+      params.forEach((item, i) => this[params[i]] = args[i])
+    }
+  }
+
+  const makeClass = (...keys) =>
+  function (...values) {
+    keys.forEach((val, idx) => this[val] = values[idx]);
+  };
+
+
+  function makeClass(...properties) {
+  return function() {
+    for (let i = 0; i < arguments.length; i++)
+      this[properties[i]] = arguments[i];
+  }
+}
+*/
