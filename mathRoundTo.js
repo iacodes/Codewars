87b2d44320e5c254022c111cad70418d03f19a58
iacodes <102711045+iacodes@users.noreply.github.com>
2022-07-15 00:22:41 -0700

    fixed method for precision of Math round method

--- mathRoundTo.js
+++ mathRoundTo.js
@@ -10,6 +10,15 @@ Math.pow(x,y) (returns x to the power of y - e.g. Math.pow(3,4) == 81)
 
 Math.roundTo = function (number, precision) {
   // TODO: Program Me
-  return this.round(this.number*this.pow(10, this.precision))/this.pow(10, this.precision)
-
+  return Math.round(number*Math.pow(10, precision))/Math.pow(10, precision)
 }
+
+
+/*Alternative solution
+Math.roundTo = (number, precision) => Number(number.toFixed(precision));
+
+Math.roundTo = function (number, precision) {
+  return Math.round(number * Math.pow(10, precision)) / Math.pow(10, precision);
+}
+
+*/
