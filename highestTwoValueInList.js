26571c346992215e682f4a29d8720b96b2cda07a
iacodes <102711045+iacodes@users.noreply.github.com>
2022-07-18 21:16:05 -0700

    initial commit, function to return highest unique values from list

---
+++ highestTwoValueInList.js
@@ -0,0 +1,59 @@
+/*
+In this kata, your job is to return the two distinct highest values in a list. If there're less than 2 unique values, return as many of them, as possible.
+
+The result should also be ordered from highest to lowest.
+
+Examples:
+
+[4, 10, 10, 9]  =>  [10, 9]
+[1, 1, 1]  =>  [1]
+[]  =>  []
+
+*/
+
+
+function twoHighest(arr) {
+  if (arr.length == 0){
+    return []
+  }
+  arr.sort((a,b) => b-a)
+  if (arr.length == 1){
+    return [arr[0]]
+  }
+  let i = 0
+  for (x in arr){
+    if (arr[0] != arr[i]){
+      return [(arr[0]),(arr[i])]
+    }
+    i++
+  }
+
+  return [(arr[0]),(arr[1])]
+}
+
+
+
+/*
+Alternative solutions
+
+function twoHighest(arr) {
+  return [...new Set(arr)].sort((a, b) => b - a).slice(0, 2)
+}
+
+const twoHighest = a => a
+  .filter((e, i) => i === a.lastIndexOf(e))
+  .sort((x, y) => y - x)
+  .slice(0, 2);
+
+
+  function twoHighest(arr) {
+    if (!arr.length) return [];
+    const max1 = Math.max(...arr);
+    let max2;
+    const filteredArr = arr.filter((el) => el !== max1);
+    if (filteredArr.length) {
+      max2 = Math.max(...filteredArr);
+    }
+    return max2 ? [max1, max2] : [max1];
+  }
+*/
