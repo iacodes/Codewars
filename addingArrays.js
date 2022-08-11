472add373f378cf1861e539507f9d700ab4e7700
iacodes <102711045+iacodes@users.noreply.github.com>
2022-08-10 23:42:41 -0700

    initial commit

--- addingArrays.js
+++ addingArrays.js
@@ -1,14 +1,47 @@
-/*Create a function with two arguments that will return an array of the first (n) multiples of (x).
+/*
+Create a function that takes an array of letters, and combines them into words in a sentence.
 
-Assume both the given number and the number of times to count will be positive numbers greater than 0.
+The array will be formatted as so:
 
-Return the results as an array*/
+[['J','L','L','M']
+,['u','i','i','a']
+,['s','v','f','n']
+,['t','e','e','']]
+The function should combine all the 0th indexed letters to create the word 'just', all the 1st indexed letters to create the word 'live', etc.
 
-function countBy(x, n) {
-  let z = [];
-  for (i = x ; i<=n ; i+=x ){
-    console.log(i)
-    z.push(x)
+Shorter words will have an empty string in the place once the word has already been mapped out (see the last element in the last element in the array).
+*/
+
+
+function arrAdder(arr) {
+  let arrLength = arr.length
+  let loopLength = arr[0].length
+  let result = []
+
+  for (i=0; i<loopLength ; i++){
+    for (j=0; j <arrLength; j++){
+      if (arr[j][i] == ""){
+        continue
+      }
+      else result.push(arr[j][i])
+    }
+    result.push(" ")
   }
-  return z;
+  return result.slice(0,-1).join("")
 }
+
+/*
+Other solutions
+function arrAdder(arr) {
+  var res = '';
+  for (var i = 0; i < arr[0].length; i++) {
+    for (var j = 0; j < arr.length; j++) {
+      res += arr[j][i];
+    }
+    res += ' ';
+  }
+  return res.trim();
+}
+
+const arrAdder = arr =>  arr[0].map((_,i)=>  arr.map((_,j)=> arr[j][i]).join('')).join(' ');
+*/
