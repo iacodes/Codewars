5bdd61b888cf169ab7b5a69b1fac043c71a7b291
iacodes <102711045+iacodes@users.noreply.github.com>
2022-08-19 23:39:58 -0700

    fixed function

--- shortestWord.js
+++ shortestWord.js
@@ -4,13 +4,6 @@ Simple, given a string of words, return the length of the shortest word(s).
 String will never be empty and you do not need to account for different data types.
 */
 
-
 function findShort(s){
-  let shortestWord = ""
-  s.split(" ").forEach( x=> {
-    if (shortestWord.length < x.length) {
-      shortestWord == x
-    }
-  })
-  return shortestWord
+    return Math.min(...s.split(" ").map(x => x.length))
 }
