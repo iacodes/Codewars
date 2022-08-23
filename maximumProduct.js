60840bb00d910a873e0b00182c0144b9a12b9aef
iacodes <102711045+iacodes@users.noreply.github.com>
2022-08-22 23:50:38 -0700

    initial commit

--- maximumProduct.js
+++ maximumProduct.js
@@ -7,11 +7,29 @@ Given an array of integers , Find the maximum product obtained from multiplying 
 function adjacentElementsProduct(array) {
     let newArr = []
     array.forEach((x,i,a) => {
-        if (i <= array.length){
+        if (i < array.length-1){
             newArr.push(x*a[i+1])
         }
         return newArr
     })
    newArr.sort((a,b) => b-a)
+   //.filter( x=> x == x+1-1 )
   return newArr[0]
 }
+
+
+/*Other solutions
+function adjacentElementsProduct(a) {
+  return Math.max(...a.map((x,i)=>x*a[i+1]).slice(0,-1))
+}
+
+
+function adjacentElementsProduct(array) {
+  let newArr = []
+  for(i=0; i < array.length-1; i++){
+    newArr.push(array[i]*array[i+1])
+  }
+  return Math.max(...newArr)
+}
+
+*/
---
+++ sortAndStar.js
@@ -0,0 +1,19 @@
+/*
+You will be given a list of strings. You must sort it alphabetically (case-sensitive, and based on the ASCII values of the chars) and then return the first value.
+
+The returned value must be a string, and have "***" between each of its letters.
+
+You should not remove or add elements from/to the array.
+
+*/
+
+function twoSort(s) {
+  return s.sort((a,b)=> a-b)[0].split("").map((x,i,a) => {
+      if (i < a.length-1){
+          return x+"***"
+      }
+      else {
+          return x
+      }
+  }).join("")
+}
