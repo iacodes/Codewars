b6e8b902e71f991ef360ab0e2feda821d95c6250
iacodes <102711045+iacodes@users.noreply.github.com>
2022-08-27 17:59:23 -0700

    remove file

--- listFiltering.js
+++
@@ -1,5 +0,0 @@
-//create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
-
-function filter_list(l) {
-  return l.filter(x => typeof(x)=="number") //fixed error where missing return, thus undefined previously during function call
-}
