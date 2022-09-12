5cff6efeb87eb5e56a73152e694630129bee015f
iacodes <102711045+iacodes@users.noreply.github.com>
2022-09-11 18:30:39 -0700

    initial commit

--- arr5addItemToArray.js
+++ arr5addItemToArray.js
@@ -1,18 +1,11 @@
 /*
-We want an array, but not just any old array, an array with contents!
+Add the value "codewars" to the websites array.
+After your code executes the websites array should == ["codewars"]
 
-Write a function that produces an array with the numbers 0 to N-1 in it.
+The websites array has already been defined for you using the following code:
 
-For example, the following code will result in an array containing the numbers 0 to 4:
-
-arr(5) // => [0,1,2,3,4]
-Note: The parameter is optional. So you have to give it a default value.
+var websites = [];
 */
 
-const arr = n => {
-  let newArr= []
-  for(i=0; i< n; i++){
-    newArr.push(i)
-  }
-  return newArr
-}
+// add the value "codewars" to the already defined websites array
+websites.push("codewars")
