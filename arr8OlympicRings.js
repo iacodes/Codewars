406e71d2c4a06581ed69b5c9f7f40b731844a859
iacodes <102711045+iacodes@users.noreply.github.com>
2022-09-16 22:45:31 -0700

    fixed function to count score correctly

--- arr8OlympicRings.js
+++ arr8OlympicRings.js
@@ -10,11 +10,12 @@ Dots over i's and any other letters don't count as rings.
 
 
 function olympicRing(a){
+  console.log(a)
   let conversion = {
     "A":1,
     "a":1,
     "B":2,
-    "b":2,
+    "b":1,
     "D":1,
     "d":1,
     "e":1,
@@ -23,15 +24,19 @@ function olympicRing(a){
     "o":1,
     "P":1,
     "p":1,
-    "R":1,
     "Q":1,
-    "q":1
+    "q":1,
+    "R":1
   }
   let countMedals = 0;
   let arr = a.split("")
-  arr.forEach( x => { if (conversion[x]==true){
+  arr.forEach( x => { if (conversion[x] != null){ //
+    //arr.forEach( x => { if (conversion[x]==true) //initially using this function which had a bug because for 2; conversion was not true / 1
+//     console.log(`Count medals is now ${countMedals}`)
     countMedals+= conversion[x]
+//     console.log(`${x} is conversion[x]`)
     return countMedals
   }})
-return Math.floor(countMedals/2) <=1 ? 'Not even a medal!' : Math.floor(countMedals/2) == 2 ? 'Bronze!' : Math.floor(countMedals/2) == 3 ?  'Silver!' : "Gold!"
+  console.log(`${Math.floor(countMedals/2)} medals`)
+  return Math.floor(countMedals/2) <=1 ? 'Not even a medal!' : Math.floor(countMedals/2) == 2 ? 'Bronze!' : Math.floor(countMedals/2) == 3 ?  'Silver!' : "Gold!"
 }
