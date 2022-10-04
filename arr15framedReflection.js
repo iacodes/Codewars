82f6b9a99e9cc4c525afca15aeb2392b0d76fcfb
iacodes <102711045+iacodes@users.noreply.github.com>
2022-10-03 22:59:01 -0700

    modified function

--- arr15framedReflection.js
+++ arr15framedReflection.js
@@ -13,10 +13,35 @@ Words in your solution should be left-aligned.
 function mirror(text){
   let mirroredText = []
 
+  let splitText = text.split(" ")
+  let numberOfStars = Math.max(...splitText.map(x => x.length))
+  let starsBeginEnd = []
+
+    console.log(splitText)
+
+  for(i = 1; i <= numberOfStars; i++){
+    starsBeginEnd.push("*")
+    if (i==numberOfStars){
+      starsBeginEnd.push("\n")
+    }
+    starsBeginEnd.join("")
+  }
+    mirroredText.push(starsBeginEnd.join(""))
+console.log(mirroredText)
+
   for (i=0; i< text.length; i++){
     mirroredText.push("*")
+      let arr = splitText[i]
+      console.log(arr)
+    // mirroredText.push(arr.split("").reverse().join(""))
+    mirroredText.push("* \n")
   }
-  
 
-  return mirroredText.join("")
+    console.log(mirroredText)
+
+  // // mirroredText.push(starsBeginEnd)
+
+
+  // // return mirroredText.join("")
+
 }
