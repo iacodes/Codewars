242737862f60f58040067e4ff2b4bdaf7b690ad9
iacodes <102711045+iacodes@users.noreply.github.com>
2022-10-06 23:55:38 -0700

    initial commit

---
+++ day6Strings.js
@@ -0,0 +1,18 @@
+/*
+Write a function that, when given a non-empty string, and positive integer X, removes every Xth character from the string. Counting should begin from the first element in the string and should continue in that pattern until the end of the string.
+*/
+
+
+function removeNthChar(str, num){
+    let arr = str.split("")
+    if (arr.length > 0){
+        for (i=0; i< arr.length; i++){
+            if (i>0 && (i+1)%num==0){
+              arr.splice(i,1)
+            }
+        }
+    }
+
+  let returnStr = arr.join("")
+  return returnStr
+}
