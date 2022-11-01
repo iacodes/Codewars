3f266681b8423926ed2ba034533bda1cb431bf0d
iacodes <102711045+iacodes@users.noreply.github.com>
2022-10-31 20:31:31 -0700

    initial commit

---
+++ heronsFormula.js
@@ -0,0 +1,22 @@
+/*
+Write function heron which calculates the area of a triangle with sides a, b, and c (x, y, z in COBOL).
+
+Heron's formula:
+
+
+
+sqrt(s∗(s−a)∗(s−b)∗(s−c))
+​
+
+where
+s=a+b+c/2
+​
+Output should have 2 digits precision.
+
+
+*/
+
+function heron(a, b, c) {
+  let s = (a+b+c)/2
+  return Math.round(Math.sqrt(s∗(s−a)∗(s−b)∗(s−c)),2);
+}
